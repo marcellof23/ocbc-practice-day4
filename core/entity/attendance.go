0d4e0c53553ba98c9a3ca37a8ce2c866d182ba9c
Marcello Faria <01faria.marcello@gmail.com>
2022-06-13 07:18:02 +0700

    chore: remove unnecessary comment

--- core/entity/attendance.go
+++ core/entity/attendance.go
@@ -8,11 +8,8 @@ import (
 
 type Attendance struct {
 	gorm.Model
-	// AttendanceID uint64 `json:"id" gorm:"primary_key"`
 	EmployeeID uint
 	Date       time.Time `json:"date"`
-	// CreatedAt  time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
-	// UpdatedAt  time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
 
 type CreateAttendanceInput struct {
--- core/entity/employee.go
+++ core/entity/employee.go
@@ -8,16 +8,12 @@ import (
 
 type Employee struct {
 	gorm.Model
-	// EmployeeID uint64    `json:"id" gorm:"primary_key"`
 	Name       string    `json:"name"`
 	BirthDate  time.Time `json:"birthDate"`
 	Address    string    `json:"address"`
 	Job        string    `json:"job"`
 	JoinDate   time.Time `json:"joinDate"`
 	Attendance []Attendance
-	// CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
-	// UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
-	// DeletedAt time.Time `json:"deleted_at"`
 }
 
 type CreateEmployeeInput struct {
